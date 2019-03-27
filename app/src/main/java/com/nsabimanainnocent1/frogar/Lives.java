3f0306e5f3d4f9f88239d3f18a3da5aed5378c08
Andrei Stoyanow <stoyanowaa@hendrix.edu>
2019-03-26 23:46:11 -0500

    Added Scorekeeping/Lives Classes
    Added classes for scorekeeping and lives

---
+++ app/src/main/java/com/nsabimanainnocent1/frogar/Lives.java
@@ -0,0 +1,14 @@
+package com.nsabimanainnocent1.frogar;
+
+public class Lives {
+
+    public int lives = 100;
+
+    public int subtractLife(int lives){
+        int livesLeft = lives - 1;
+        return livesLeft;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/nsabimanainnocent1/frogar/Scorekeeping.java
@@ -0,0 +1,7 @@
+package com.nsabimanainnocent1.frogar;
+
+public class Scorekeeping {
+
+    public int score = 0;
+    
+}
