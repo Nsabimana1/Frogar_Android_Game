678f12bf7ea9ee96b23cbb04051ae852fec995af
Andrei Stoyanow <stoyanowaa@hendrix.edu>
2019-03-27 22:10:11 -0500

    Updated Lives/Scorekeeping classes
    Added functions to lives class (for subtracting lives) and scorekeeping class

--- app/src/main/java/com/nsabimanainnocent1/frogar/Scorekeeping.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/Scorekeeping.java
@@ -3,5 +3,11 @@ package com.nsabimanainnocent1.frogar;
 public class Scorekeeping {
 
     public int score = 0;
-    
+
+    public void addPoint() {
+        score += 1;
+    }
+
+    public int getScore(){return score;}
+
 }
