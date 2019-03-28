9b5b387f64c83bc58c0adbc72cec76ce0f73dba6
Andrei Stoyanow <stoyanowaa@hendrix.edu>
2019-03-27 23:02:17 -0500

    Updated Scorekeeping
    Added methods to Scorekeeping for when the frog reaches the left and right sides of the screen. Pending to change based on GUI.

--- app/src/main/java/com/nsabimanainnocent1/frogar/gameObjects/Scorekeeping.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/gameObjects/Scorekeeping.java
@@ -1,13 +1,43 @@
 package com.nsabimanainnocent1.frogar.gameObjects;
 
+import android.view.Display;
+import android.view.WindowManager;
+
+import com.nsabimanainnocent1.frogar.GameScreen;
+
 public class Scorekeeping {
 
     public int score = 0;
+    public int leftScoreZone;
+    public boolean reachedLeftZone = true;
+    public int rightScoreZone;
+    public boolean reachedRightZone = false;
+    public int frogPosition;
 
     public void addPoint() {
         score += 1;
     }
 
-    public int getScore(){return score;}
+    public int getScore() { return score; }
+
+    public void checkLeftScoreZone(){
+        if(frogPosition == leftScoreZone){
+            reachedLeftZone = true;
+            reachedRightZone = false;
+            addPoint();
+        }else{
+
+        }
+    }
+
+    public void checkRightScoreZone(){
+        if(frogPosition == rightScoreZone){
+            reachedRightZone = true;
+            reachedLeftZone = false;
+            addPoint();
+        }else{
+            
+        }
+    }
 
 }
