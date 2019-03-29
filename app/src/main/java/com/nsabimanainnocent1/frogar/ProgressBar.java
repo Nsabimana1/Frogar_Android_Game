fb4669d64edc955db3fa0035dd987b8f867dbbed
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-03-29 11:15:22 -0500

    Adding Icons for the UI

--- app/src/main/java/com/nsabimanainnocent1/frogar/ProgressBar.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/ProgressBar.java
@@ -1,4 +1,28 @@
 package com.nsabimanainnocent1.frogar;
 
 public class ProgressBar {
+    public int  health = 4;
+
+
+    //This method gets called whenever score is Subtracted
+    public int LoseLife(){
+        return health-1;
+    }
+
+    // All of the progress bar images will be in the same place
+    // But they will be hidden and unhidden based on the health.
+
+    public void ProgressLoser() {
+        if (health == 3) {
+            //switch to image view of progress bar 2
+        }
+        if (health == 2) {
+            //switch to image view of  progress bar 3
+        }
+        else{
+            //switch to image view of  progress bar 4
+        }
+    }
+
+
 }
