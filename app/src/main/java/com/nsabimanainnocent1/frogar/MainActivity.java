11522c04e1ff7367bb72e4a67f85b2b36822a665
Nsabimana1 <nsabimanainnocent1@gmail.com>
2019-03-30 13:04:35 -0500

    added more UI code

--- app/src/main/java/com/nsabimanainnocent1/frogar/MainActivity.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/MainActivity.java
@@ -1,13 +1,26 @@
 package com.nsabimanainnocent1.frogar;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent gameIntent = new Intent(MainActivity.this, GameScreen.class);
+                startActivity(gameIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
