12b1ae8a6d7fa6526726580a0dfd1b0b8e54850a
Nsabimana1 <nsabimanainnocent1@gmail.com>
2019-04-07 00:23:44 -0500

    bug fixed

--- app/src/main/java/com/nsabimanainnocent1/frogar/movement/GameStateUpdater.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/movement/GameStateUpdater.java
@@ -1,17 +1,20 @@
 package com.nsabimanainnocent1.frogar.movement;
 
+import android.content.Intent;
 import android.widget.TextView;
 
 public class GameStateUpdater {
     private TextView scoreView, healthView, gameLevelView;
     private Integer gameScore, healthScore, gameLevelValue;
     private CollisionDetector collisionDetector;
+    private Intent intent;
 
-    public GameStateUpdater(TextView scoreView, TextView healthView, TextView gameLevelView){
+    public GameStateUpdater(TextView scoreView, TextView healthView, TextView gameLevelView, Intent intent){
         this.scoreView = scoreView;
         this.healthView =  healthView;
         this.gameLevelView = gameLevelView;
         this.gameScore = 0; this.healthScore = 5; this.gameLevelValue = 1;
+        this.intent = intent;
     }
 
     public void setCollisionDetector(CollisionDetector collisionDetector) {
@@ -25,6 +28,14 @@ public class GameStateUpdater {
     }
 
     public void updateUI(){
+
+        intent.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                restartGame();
+            }
+        });
+
         this.scoreView.setText(gameScore.toString());
         this.healthView.setText(healthScore.toString());
         this.gameLevelView.setText(gameLevelValue.toString());
