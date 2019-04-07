b27714f9b8386d385bf3981b0e208e9e2945b299
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-04-07 04:03:02 -0500

    Added Methods
    Have created score,level, reset, and progressbar methods.

--- app/src/main/java/com/nsabimanainnocent1/frogar/GameScreen.java
+++ app/src/main/java/com/nsabimanainnocent1/frogar/GameScreen.java
@@ -1,6 +1,8 @@
 package com.nsabimanainnocent1.frogar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,13 +16,14 @@ import com.nsabimanainnocent1.frogar.movement.Game;
 import com.nsabimanainnocent1.frogar.movement.GameStateUpdater;
 
 public class GameScreen extends AppCompatActivity {
-    public ImageView imageViewRoad, frogImage, carImage1, carImage2;
+    public ImageView imageViewRoad, frogImage, carImage1, carImage2,pBFull, pBHalf, pbEmpty;
     private TextView healthTitleView, scoreTitleView, scoreValueView, gameLevelView;
     private Button playButton, restartButton;
     private Integer scoreValue = 0;
     private Integer imageX = 0, imageY = 0;
     private Game game;
     private GameStateUpdater gameStateUpdater;
+    private Integer lives = 3;
 
     private TextView testingView;
 
@@ -44,6 +47,13 @@ public class GameScreen extends AppCompatActivity {
                 testingView.setText(imageX.toString() + ", " + imageY.toString());
             }
         });
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetButton();
+                game.starGame();
+            }
+        });
 
 
 //        imageX = imageViewRoad.getX();
@@ -65,6 +75,9 @@ public class GameScreen extends AppCompatActivity {
 
     private void setComponents(){
         imageViewRoad = findViewById(R.id.imageViewRoad);
+        pbEmpty = findViewById(R.id.ProgressBarEmpty);
+        pBFull = findViewById(R.id.ProgressBarFull);
+        pBHalf = findViewById(R.id.ProgressBarHalf);
         healthTitleView = findViewById(R.id.heathTitle);
         scoreTitleView = findViewById(R.id.scoreTitle);
         scoreValueView = findViewById(R.id.scoreValue);
@@ -88,6 +101,11 @@ public class GameScreen extends AppCompatActivity {
         game.setCarMovement();
         game.setRoadDimension(imageViewRoad.getHeight(), imageViewRoad.getWidth());
         game.setCollisionDetector();
+        pBHalf.setVisibility(View.INVISIBLE);
+        pbEmpty.setVisibility(View.INVISIBLE);
+    }
+    public void resetButton(){
+        this.initializeObjects();
     }
 
     public void resetCarLocation(ImageView car, ImageView parent){
@@ -96,7 +114,51 @@ public class GameScreen extends AppCompatActivity {
             car.setY(0);
         }
     }
+    public void ProgressBar(Boolean collision){
+        if(collision && lives == 2){
+            pBFull.setVisibility(View.INVISIBLE);
+            pBHalf.setVisibility(View.VISIBLE);
+            lives -= 1;
+            scoreValue -= 1;
+        }
+        else if(collision && lives == 1){
+            pBHalf.setVisibility(View.INVISIBLE);
+            pbEmpty.setVisibility(View.VISIBLE);
+            lives -=1;
+            scoreValue -= 1;
+        }
+        else if(lives == 0) {
+            AlertDialog.Builder loseAlert = new AlertDialog.Builder(GameScreen.this);
+            loseAlert.setMessage("Your Score was:" + scoreValue+" would you like to try again?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            resetButton();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    });
 
+        }
+
+    }
+    public void updateLevel() {
+        int level = 0;
+        if (scoreValue % 10 == 10) {
+            gameLevelView.setText("Level:" + level);
+            level +=1;
+        }
+    }
+    public int updateScore(boolean hasReachedGoal){
+        scoreTitleView.setText("Score:" + scoreValue);
+        return scoreValue += 10;
+    }
     public static void updateUI(){
     }
 }
